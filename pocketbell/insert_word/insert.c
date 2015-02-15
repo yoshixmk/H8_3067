fd64160b4e94fb495666d09f92d910ffb26071f7
yoshihiro <ayh_black@yahoo.co.jp>
2015-02-15 22:14:57 +0900

    insert 2times Enter => dataText

--- pocketbell/insert_word/insert.c
+++ pocketbell/insert_word/insert.c
@@ -2,15 +2,35 @@
 
 int main(void)
 {
-	int i;
+	int i, enter_count = 0;
+	char c;
 	char insert1, insert2;
 	char text_buffer[256];
 	char text[16][2];
-	gets(text_buffer);
+/*gets(text_buffer);*/
+	for(i=0; i < 256; i++){
+		c = getchar();
+		if(c == '\n'){
+			enter_count++;
+			i--; /*‰üs•¶Žš‚ÍŠÜ‚ß‚È‚¢‚½‚ß*/
+		}
+		else{
+			enter_count = 0;
+			text_buffer[i] = c;
+		}
+		if(enter_count == 2){
+			text_buffer[++i] = '\0';
+			break;
+		}
+	}
 	for(i=0; i<16; i+=2){
-		insert1 = text_buffer[i];
-		insert2 = text_buffer[i+1];
-		printf("%c%c\n",insert1,insert2);
+		if(text_buffer[i] != '\0')
+			insert1 = text_buffer[i];
+		else break;
+		if(text_buffer[i+1] != '\0')
+			insert2 = text_buffer[i+1];
+		else break;
+		printf("%i%c%c\n",i,insert1,insert2);
 	}
 	return 0;
 }
